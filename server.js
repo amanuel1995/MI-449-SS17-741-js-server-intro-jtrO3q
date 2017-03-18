c6f75ec3af03d0061a372a095581d5869cc6df88
Amanuel <amaniye1@live.com>
2017-03-18 17:21:20 -0400

    Update the port by using the env variable

--- server.js
+++ server.js
@@ -52,5 +52,5 @@ var server = http.createServer(function (request, response) {
 })
 
 // Listen on port 8080, so that we can reach the app at localhost:8080
-var port = 8080
+var port = process.env.PORT || 8080
 server.listen(port)
